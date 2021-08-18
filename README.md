c5ccc926c2eab35fdd15637a5579129748f9e9fa
Emre Demirbag <emredemirbag@protonmail.com>
2021-08-18 11:48:27 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Clue Catcher( Simple Web Parser)
+
+## Purpose: 
+This code captures everyday questions (clues)  of New York Times Mini Crossword Puzzle (https://www.nytimes.com/crosswords/game/mini) 
+
+It checks for the html tags to get the information of each clues.
+It creates a dictionary with the above clue information.
+It converts the python objects into json object and export it to clues.json file.
+It exports a list of clues to text file
+
+## Environment Requirement & Dependencies
+1) python 3.8.2
+2) beautifulsoup4==4.9.3
+3) bs4==0.0.1
+4) certifi==2021.5.30
+5) charset-normalizer==2.0.4
+6) idna==3.2
+7) lxml==4.6.3
+8) requests==2.26.0
+9) soupsieve==2.2.1
+10) urllib3==1.26.6
+
+## Installing Depedencies
+1) pip install bs4
+2) pip install requests
+3) pip install lxml
---
+++ main.py
@@ -0,0 +1,80 @@
+# Simple HTML Parser, coded by Emre Demirbag https://github.com/emre-demirbag
+# New York Times Mini Crossword Puzzle,by Joel Fagliano,(https://www.nytimes.com/crosswords/game/mini)
+#requests:(http://docs.python-requests.org/en/master/),
+#soup:(https://www.crummy.com/software/BeautifulSoup/bs4/doc/)
+
+
+# Import the required modules
+import requests
+from bs4 import BeautifulSoup as Soup
+
+
+
+session = requests.Session()
+
+
+
+def getHTML(url):
+
+# session.get(url) returns a response that is saved
+# in a response object called resp.
+
+    resp = session.get(url)
+
+# BeautifulSoup will create a
+# parsed tree in soup.
+
+    soup = Soup(resp.content, "lxml")
+
+    return soup
+
+
+
+def getColoumns(soup):
+    # soup.find_all finds the div's, all having the same
+    # class "ClueList-wrapper--3m-kd" that is
+    # stored NyTimes Mini Crosswords web site
+    # https://www.nytimes.com/crosswords/game
+
+    cluewrap = soup.find_all("div", {"class": "ClueList-wrapper--3m-kd"})
+    # Initialise the required variables
+    html = ""
+
+
+# Iterate cluewrap and clutext check for the html tags
+# to get the information of each clues.
+
+    for i in cluewrap:
+
+        clueListTitle = i.findAll("h3")
+
+        title = i.find('h3', attrs={'class': 'ClueList-title--1-3oW'})
+
+        html += "=== " + clueListTitle[0].text + " ===" + "\n"
+
+        cluetext = i.findAll("li")
+
+        for j in cluetext:
+            span = j.findAll("span")
+            title[0] = title.text.strip()
+            html += " " + span[0].text.strip() + " " + span[1].text.strip() + "\n"
+
+    print(html)
+
+    return html
+
+# Main Function
+if __name__ == "__main__":
+    url = "https://www.nytimes.com/crosswords/game/mini"
+    # Enter the url of website
+    s = getHTML(url)
+    # Function will return a list of clues
+    html = getColoumns(s)
+
+
+    # export a list of clues to text file
+    file_r = open("text.txt", "w")
+    file_r.write(html)
+    file_r.close()
+    print("Created text file")
+
